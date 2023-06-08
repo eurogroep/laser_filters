ad40d8b7fee0a283b44f345029bc9a070d43bb9e
Yannick de Hoop <dehoopyannick@gmail.com>
2023-06-08 16:59:04 +0200

    whiteline

--- src/intensity_filter.cpp
+++ src/intensity_filter.cpp
@@ -108,6 +108,7 @@ bool LaserScanIntensityFilter::update(const sensor_msgs::LaserScan& input_scan, 
   auto update_elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
 
   ROS_DEBUG_NAMED("LaserScanIntensityFilter", "LaserScanIntensityFilter update took %lu microseconds", update_elapsed);
+  
   return true;
 }
 
